6d56ee7ccc7048fc60c29a2bb54370e6c390ca42
Rowlynn Huang <30716814+Rowlynn@users.noreply.github.com>
2019-03-31 17:09:55 +0800

    Update cousera.R

--- cousera.R
+++ cousera.R
@@ -1,7 +1,70 @@
-x<-vector(mode="numeric",length=0)
+x1<-vector(mode="numeric",length=0)
 for(i in 1:165)
 {
-  x[i]<-i
+  x1[i]<-i
 }
 p1=(1:165)
-plot(p1,x,main="rank")
+plot(p1,x1,main="rank1")
+x2<-vector(mode="numeric",length=0)
+for(i in 1:528)
+{
+  x2[i]<-i
+}
+p2=(1:528)
+plot(p2,x2,main="rank2")
+x3<-vector(mode="numeric",length=0)
+for(i in 1:927)
+{
+  x3[i]<-i
+}
+p3=(1:927)
+plot(p3,x3,main="rank3")
+x4<-vector(mode="numeric",length=0)
+for(i in 1:600)
+{
+  x4[i]<-i
+}
+p4=(1:600)
+plot(p4,x4,main="rank5")
+x5<-vector(mode="numeric",length=0)
+for(i in 1:354)
+{
+  x5[i]<-i
+}
+p5=(1:354)
+plot(p5,x5,main="rank5")
+x6<-vector(mode="numeric",length=0)
+for(i in 1:638)
+{
+  x6[i]<-i
+}
+p6=(1:638)
+plot(p6,x6,main="rank6")
+x7<-vector(mode="numeric",length=0)
+for(i in 1:771)
+{
+  x7[i]<-i
+}
+p7=(1:771)
+plot(p7,x7,main="rank7")
+x8<-vector(mode="numeric",length=0)
+for(i in 1:54)
+{
+  x8[i]<-i
+}
+p8=(1:54)
+plot(p8,x8,main="rank8")
+X9<-vector(mode="numeric",length=0)
+for(i in 1:771)
+{
+  x9[i]<-i
+}
+p9=(1:158)
+plot(p9,x9,main="rank9")
+x10<-vector(mode="numeric",length=0)
+for(i in 1:771)
+{
+  x10[i]<-i
+}
+p10=(1:771)
+plot(p10,x10,main="rank10")
